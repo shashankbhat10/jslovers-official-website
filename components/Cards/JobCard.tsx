7c12427d3eb85cc8552a723c0e74025bde741a42
Shashank Bhat <s.2205.b@gmail.com>
2024-07-25 16:54:38 -0700

    redirect to job page on job card's view button click

--- components/Cards/JobCard.tsx
+++ components/Cards/JobCard.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/Button";
 import { JobData } from "@/types";
+import Link from "next/link";
 
 interface JobCardProps extends JobData {
   className?: string;
@@ -15,12 +18,13 @@ const JobCard = ({
   role,
   company_logo,
   className,
+  id,
 }: JobCardProps) => {
   return (
     <div
       className={cn(
-        "border-solid border-border-card border-[3px] rounded-[10px] py-6 px-3",
-        className
+        "border-border-card rounded-[10px] border-[3px] border-solid px-3 py-6",
+        className,
       )}
     >
       <Image
@@ -30,45 +34,47 @@ const JobCard = ({
         alt={company_name}
         loading="lazy"
       />
-      <div className="my-3 border-[1.5px] border-border-heading" />
+      <div className="border-border-heading my-3 border-[1.5px]" />
       <div className="flex flex-col gap-[10px]">
         <div className="flex items-center justify-between">
           <p className="font-medium">{company_name}</p>
           {is_remote && (
-            <p className="font-medium text-sm py-2 px-3 bg-background-gray rounded">
+            <p className="bg-background-gray rounded px-3 py-2 text-sm font-medium">
               Remote
             </p>
           )}
         </div>
-        <p className="font-semibold text-2xl">{role}</p>
-        <div className="flex gap-x-2 items-center">
+        <p className="text-2xl font-semibold">{role}</p>
+        <div className="flex items-center gap-x-2">
           <p className="text-text-label-2 font-medium">Location</p>
           <p>
             <span className="text-text-sub-heading">{location?.city}</span>,{" "}
             {location?.country}
           </p>
         </div>
-        <div className="flex gap-x-2 items-center">
+        <div className="flex items-center gap-x-2">
           <p className="text-text-label-2 font-medium">Experience</p>
           <p className="text-text-sub-heading">{experience}</p>
         </div>
       </div>
-      <button
-        className={cn(
-          buttonVariants(),
-          "border-solid border-2 border-border mt-[10px]"
-        )}
-      >
-        View Details
-        <Image
-          src="/assets/icons/right_arrow.svg"
-          width="11"
-          height="11"
-          alt=""
-          className="ml-1"
-          loading="lazy"
-        />
-      </button>
+      <Link href={`/jobs/${id}`}>
+        <button
+          className={cn(
+            buttonVariants(),
+            "mt-[10px] border-2 border-solid border-border",
+          )}
+        >
+          View Details
+          <Image
+            src="/assets/icons/right_arrow.svg"
+            width="11"
+            height="11"
+            alt=""
+            className="ml-1"
+            loading="lazy"
+          />
+        </button>
+      </Link>
     </div>
   );
 };
